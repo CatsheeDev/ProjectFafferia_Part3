73822fa20cc9fc2e4e4de8b2110ba798d5d7f495
Cats <cheesychilli0@gmail.com>
2024-08-27 06:51:57 +0100

    More Player Controls (i think?)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,25 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent (typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
+namespace Player.Controller
 {
-    private InputAction moveAction; 
-
-    void Start()
+    [RequireComponent (typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour
     {
-        moveAction = InputSystem.actions.FindAction("Moave"); 
-
-        if (moveAction == null)
-        {
-            ErrorHandler.LoadErrorScene("Couldn't resolve Move input action");
-            return; 
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        private InputAction moveAction; 
+        private CharacterController body; 
         
+        [SerializeField, Header("Speed")] private float PlayerSpeed; //cant combine all 4 cuz unity serialization sukcs
+        [SerializeField] private float BaseSpeed, RunSpeed, SlowSpeed; 
+        
+        private void Start()
+        {
+            moveAction = InputSystem.actions.FindAction("Move"); 
+            body = GetComponent<CharacterController>();
+            
+            if (moveAction == null)
+            {
+                ErrorHandler.LoadErrorScene("Couldn't resolve Move input action // PlayerController // Start");
+                return; 
+            }
+        }
+    
+        // Update is called once per frame
+        private void Update()
+        {
+            Vector2 moveInfo = moveAction.ReadValue<Vector2>();
+    
+            body.Move(moveInfo * PlayerSpeed * Time.DeltaTime); //NOT good. CHANGE THIS!!!!!!!!!!! -love, me :)
+        }
     }
 }
