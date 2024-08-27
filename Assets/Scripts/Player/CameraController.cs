b37d4d32cc29f951001639f167ae892b76b98fff
Cats <120604687+CatsheeDev@users.noreply.github.com>
2024-08-27 15:16:24 +0100

    Base Movement, Cinemachine, ReadOnlyAttribute
    i didnt need sleep

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+namespace Player.Controller.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,33 +3,86 @@ using UnityEngine.InputSystem;
 
 namespace Player.Controller
 {
-    [RequireComponent (typeof(CharacterController))]
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
-        private InputAction moveAction; 
-        private CharacterController body; 
-        
-        [SerializeField, Header("Speed")] private float PlayerSpeed; //cant combine all 4 cuz unity serialization sukcs
-        [SerializeField] private float BaseSpeed, RunSpeed, SlowSpeed; 
-        
+        private InputAction moveAction;
+        private InputAction lookAction;
+
+        private Vector2 moveInput;
+        private Vector2 lookInput;
+
+        private CharacterController body;
+
+        [SerializeField, ReadOnlyAttribute, Header("Speed")] private float PlayerSpeed;
+        [SerializeField, Space] private float BaseSpeed;
+        [SerializeField] private float RunSpeed, SlowSpeed;
+
+        [SerializeField, Header("Camera Settings")] private float Sensitivity;
+        private Quaternion PlayerRotation;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (Application.isPlaying) return;
+            PlayerSpeed = BaseSpeed;
+        }
+#endif 
+
         private void Start()
         {
-            moveAction = InputSystem.actions.FindAction("Move"); 
+            moveAction = InputSystem.actions.FindAction("Move");
+            lookAction = InputSystem.actions.FindAction("Look");
             body = GetComponent<CharacterController>();
-            
-            if (moveAction == null)
+            PlayerRotation = transform.rotation;
+
+            if (moveAction == null || lookAction == null)
             {
-                ErrorHandler.LoadErrorScene("Couldn't resolve Move input action // PlayerController // Start");
-                return; 
+                ErrorHandler.LoadErrorScene("Couldn't resolve Move or Look input action // PlayerController // Start");
+                return;
             }
         }
-    
-        // Update is called once per frame
+
         private void Update()
         {
-            Vector2 moveInfo = moveAction.ReadValue<Vector2>();
-    
-            body.Move(moveInfo * PlayerSpeed * Time.DeltaTime); //NOT good. CHANGE THIS!!!!!!!!!!! -love, me :)
+            moveInput = moveAction.ReadValue<Vector2>();
+            lookInput = lookAction.ReadValue<Vector2>();
+        }
+
+        private void LateUpdate()
+        {
+            if (!lookInput.Equals(Vector2.zero))
+            {
+                Look(lookInput);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!moveInput.Equals(Vector2.zero))
+            {
+                Move(moveInput);
+            }
+        }
+
+        private void Look(Vector2 direction)
+        {
+            if (!direction.Equals(Vector2.zero))
+            {
+                Quaternion deltaRotation = Quaternion.Euler(0f, Sensitivity * Time.timeScale * direction.x, 0f);
+                PlayerRotation *= deltaRotation;
+
+                transform.rotation = PlayerRotation;
+            }
+        }
+
+
+        public void Move(Vector2 direction)
+        {
+            Vector3 localInput = transform.TransformDirection(new Vector3(direction.x, 0, direction.y));
+            Vector3 projectedVector = new Vector3(localInput.x, 0, localInput.z);
+
+            body.Move(PlayerSpeed * Time.deltaTime * projectedVector.normalized);
         }
     }
 }
